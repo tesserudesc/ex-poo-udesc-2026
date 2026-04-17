2d5327fb3ac5f6c65d8ea6aa5b54a32d6264d850
Fernando <fernando.tesser@edu.udesc.br>
2026-04-16 21:38:38 -0300

    Exercicio 1 - Classe Aluno e respectivo teste

---
+++ src/exercicio1oo/classes/Aluno.java
@@ -0,0 +1,8 @@
+package exercicio1oo.classes;
+
+class Aluno {
+    String matricula;
+    String nome;
+    int idade;
+    int nota1, nota2, nota3, nota4;
+}
---
+++ src/exercicio1oo/classes/TestaAluno.java
@@ -0,0 +1,48 @@
+package exercicio1oo.classes;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TestaAluno {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Aluno fulano = new Aluno();
+
+        System.out.print("Nome: ");
+        fulano.nome = leitor.nextLine();
+
+        System.out.print("Matricula: ");
+        fulano.matricula = leitor.nextLine();
+
+        System.out.print("idade: ");
+        fulano.idade = leitor.nextInt();
+
+        System.out.print("nota1: ");
+        fulano.nota1 = leitor.nextInt();
+
+        System.out.print("nota2: ");
+        fulano.nota2 = leitor.nextInt();
+
+        System.out.print("nota3: ");
+        fulano.nota3 = leitor.nextInt();
+
+        System.out.print("nota4: ");
+        fulano.nota4 = leitor.nextInt();
+
+        System.out.println("\nDados do Aluno");
+        System.out.println("-------------");
+        System.out.println("Nome: " + fulano.nome);
+        System.out.println("Matricula: " + fulano.matricula);
+        System.out.println("Idade: " + fulano.idade);
+        System.out.println("nota1: " + fulano.nota1);
+        System.out.println("nota2: " + fulano.nota2);
+        System.out.println("nota3: " + fulano.nota3);
+        System.out.println("nota4 " + fulano.nota4);
+
+
+        leitor.nextLine();
+
+        System.out.println("Dados do aluno");
+        System.out.println(fulano.nome);
+    }
+}
